185b034bb7a59dcfe74a1803d9a53e6c61f97ef7
RabahSend <rabahnini3F@gmail.com>
2022-10-03 16:09:40 +0200

    Add ASCII function in the ASCII cpp file

---
+++ ASCII.cpp
@@ -0,0 +1,20 @@
+#include "../includes/TextGame.hpp"
+
+void printASCII(std::string FileName) {
+
+	std::string line = "";
+	std::fstream inFile;
+	inFile.open(FileName);
+
+	if (inFile.is_open()){
+		while (getline(inFile, line)) {
+			std::cout << "		" << line << std::endl;
+		}
+	}
+
+	else {
+		std::cerr << "impossible, fichier probablement corrompu (lol le tocard)" << std::endl;
+	}
+
+	inFile.close();
+}
